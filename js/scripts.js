646613b14c2d94355dfb282b25ea8979d95c46b5
Nathan Fletcher <nathan.fletcher@gmail.com>
2021-08-12 13:35:52 -0700

    Add submit button and if/else statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,22 @@
 $(document).ready(function () {
 
-  $('#height-form').submit(function () {
-    const feet = parseInt(('#feet-input').val());
-    const inches = parseInt(('#inches-input').val());
+  $('#height-form').submit(function (e) {
+    e.preventDefault();
+    const feet = parseInt($('#feet-input').val());
+    const inches = parseInt($('#inches-input').val());
+    // console.log(feet, inches)
+    const height = feet * 12 + inches;
+
+    if (height < 63) {
+      $('#short').show();
+      $('#everybody').show();
+    } else if (height > 72) {
+      $('#tall').show();
+      $('#everybody').show();
+    } else {
+      $('#medium').show();
+      $('#everybody').show();
+    }
 
 
   });
